0a87d33ad0f27c71ef68959bc61a3afeea4f02d3
CNRP <49189988+CNRP@users.noreply.github.com>
2019-06-09 17:46:48 +0100

    backup update
    backup update

--- static/js/guide-select.js
+++
@@ -1,9 +0,0 @@
-$(document).on('click', '#click-me', () => {
-    $('#info-modal').addClass("show");
-})
-
-$(document).ready(function(){
-	$(".modal-dialog .close").click(function(){
-		$(this).closest(".modal-dialog").removeClass("show"); 
-	});
-});
